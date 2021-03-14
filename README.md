e0760691ad59db2b97a9f8f13fa67a2fd4d632ee
pbrotoisworo <panji.p.broto@gmail.com>
2021-03-14 11:26:59 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# s1-tops-split-analyzer
-Python interface for visualizing and analyzing S1-TOPS-SPLIT data that is used in ESA SNAP Software
+# S1-TOPS-SPLIT Analyzer
+
+This code intends to help users easily work with the S1-TOPS-SPLIT function in SNAP software by parsing the XML metadata of Sentinel-1 satellite data.
+
+Using S1-TOPS-SPLIT Analyzer you are able to:
+* View all subswaths at the same time
+* Save S1-TOPS-SPLIT data as a shapefile, JSON, or CSV
+* View and interact with S1-TOPS-SPLIT data using a webmap. In addition, you can add a polygon to visualize its extent with regards to the S1-TOPS-SPLIT data
+
+![alt](sample_webmap.png)
+
+## Installation
+Recommended Python version is 3.8. Minimum Python version is 3.6 but this is untested.
+
+Install Python dependencies
+
+`pip install pandas geopandas shapely folium`
+
+## Usage
+Copy `topsplitanalyser.py` to your work directory. Then import the Class by typing:
+
+```python
+from topsplitanalyser import TopSplitAnalyser
+```
+
+Below is a sample of using `TopsSplitAnalyzer` to create a shapefile and visualize on a webmap. To visualize on a webmap you need to be using Jupyter Notebook.
+
+```python
+# Create object
+s1 = TopsSplitAnalyzer('S1_image.zip', target_subswaths=['iw1, iw2, iw3'])
+
+# Write to shapefile
+s1.to_shapefile('data.shp')
+
+# Get JSON
+s1.to_json()
+
+# Write to CSV
+s1.to_csv('output.csv')
+
+# Visualize on a webmap with additional polygon
+s1.visualize_webmap(polygon='mask.shp')
+```
---
+++ TopSplitAnalyzer.py
@@ -0,0 +1,242 @@
+import os
+import re
+from zipfile import ZipFile
+import xml.etree.ElementTree as ET
+import folium
+from shapely.geometry import Polygon, Point, MultiPolygon
+import geopandas as gpd
+import pandas as pd
+from typing import Union
+
+
+class TopsSplitAnalyzer:
+
+    def __init__(self, image: str, target_subswath: Union[None, list] = None, polarization='vv', verbose=True):
+        """
+        Class to interpret and visualize S1-TOPS-SPLIT data as seen in ESA SNAP software.
+        
+        :param image: Path of ZIP file containing Sentinel-1 imagery
+        :param target_subswath: String or list of subswaths to load
+        :param polarization: Polarization of imagery, defaults to 'vv'
+        :param verbose: Print statements, defaults to True
+        """
+        # Load and check user inputs
+        if isinstance(target_subswath, str):
+            if target_subswath.lower() not in ['iw1', 'iw2', 'iw3']:
+                raise Exception(f'Target subswath "{target_subswath.lower()}" not a valid subswath')
+        if target_subswath is None:
+            target_subswath = ['iw1', 'iw2', 'iw3']
+        self._image = image
+        self._target_subswath = target_subswath
+        self.polarization = polarization
+        self._verbose = verbose
+
+        # Initial functions
+        self.archive = ZipFile(self._image)
+        if self._verbose:
+            print(f'Loaded ZIP file: {os.path.basename(self._image)}')
+
+        # Declare variables
+        self._metadata = None
+        self.metadata_file_list = []
+        self.total_num_bursts = None
+        self.df = None
+
+    # Get metadata
+    def _load_metadata(self, target_subswath=None, target_polarization='vv'):
+        """
+        Load XML data
+        :param target_subswath: Desired subswath for metadata extraction
+        :param target_polarization: Desired polarization for metadata extraction
+        :return: ZipFle object which contains XML file to be loaded into ElementTree
+        """
+
+        if not target_subswath:
+            target_subswath = self._target_subswath
+
+        assert isinstance(target_subswath, str) is True, 'Expected string for _load_metadata'
+
+        # Get file list
+        archive_files = self.archive.namelist()
+
+        # Get metadata files
+        regex_filter = r's1(a|b)-iw\d-slc-(vv|vh)-.*\.xml'
+
+        # Reset metadata list before loading
+        self.metadata_file_list = []
+        for item in archive_files:
+            if 'calibration' in item:
+                continue
+            match = re.search(regex_filter, item)
+            if match:
+                self.metadata_file_list.append(item)
+
+        target_file = None
+        for item in self.metadata_file_list:
+            if target_subswath in item and target_polarization in item:
+                target_file = item
+        if not target_file:
+            raise Exception('No XML metadata file found')
+
+        # Open XML
+        metadata = self.archive.open(target_file)
+        self._metadata = metadata
+
+        self._check_metadata_loaded = True
+
+    def _parse_location_grid(self):
+        """
+        Parse the XML metadata and get the coordinate data stored in geolocationGrid
+        """
+
+        tree = ET.parse(self._metadata)
+        root = tree.getroot()
+
+        # Get subswath and burst coordinates
+        lines = []
+        coord_list = []
+        for grid_list in root.iter('geolocationGrid'):
+            for point in grid_list:
+                for item in point:
+                    lat = item.find('latitude').text
+                    lon = item.find('longitude').text
+                    line = item.find('line').text
+                    lines.append(line)
+                    coord_list.append((float(lat), float(lon)))
+        if self.total_num_bursts is None and self._verbose:
+            print(f'Loaded location grid with {len(set(lines)) - 1} bursts')
+        self.total_num_bursts = len(set(lines)) - 1
+
+        return coord_list
+
+    def _parse_subswath_geometry(self, coord_list):
+        """
+        Create geometry from location grid XML data
+        :param coord_list: Input coord list loaded from geolocationGrid metadata
+        :return:
+        """
+        def get_coords(index, coord_list):
+            coord = coord_list[index]
+            return coord[0], coord[1]
+
+        bursts_dict = {}
+        top_right_idx = 0
+        top_left_idx = 20
+        bottom_left_idx = 41
+        bottom_right_idx = 21
+
+        for burst_num in range(1, self.total_num_bursts + 1):
+            # Create polygon
+            burst_polygon = Polygon(
+                [Point(get_coords(top_right_idx, coord_list)[1], get_coords(top_right_idx, coord_list)[0]),  # Top right
+                 Point(get_coords(top_left_idx, coord_list)[1], get_coords(top_left_idx, coord_list)[0]),  # Top left
+                 Point(get_coords(bottom_left_idx, coord_list)[1], get_coords(bottom_left_idx, coord_list)[0]),
+                 # Bottom left
+                 Point(get_coords(bottom_right_idx, coord_list)[1], get_coords(bottom_right_idx, coord_list)[0])
+                 # Bottom right
+                 ]
+            )
+
+            top_right_idx += 21
+            top_left_idx += 21
+            bottom_left_idx += 21
+            bottom_right_idx += 21
+            
+            bursts_dict[burst_num] = burst_polygon
+
+        return bursts_dict
+
+    def _create_subswath_geometry(self):
+        """
+        Create geodataframe from the XML metadata
+        :return:
+        """
+
+        if isinstance(self._target_subswath, list):
+            df_all = gpd.GeoDataFrame(columns=['subswath', 'burst', 'geometry'])
+            for subswath in self._target_subswath:
+                self._load_metadata(subswath, self.polarization)
+                coord_list = self._parse_location_grid()
+                subswath_geom = self._parse_subswath_geometry(coord_list)
+                df = gpd.GeoDataFrame(
+                    [[subswath.upper()] * len(subswath_geom), subswath_geom.keys(), subswath_geom.values()],
+                    crs='EPSG:4326').T
+                df.columns = ['subswath', 'burst', 'geometry']
+                df_all = gpd.GeoDataFrame(pd.concat([df_all, df]), crs='EPSG:4326')
+        else:
+            # Write one band only
+            self._load_metadata()
+            coord_list = self._parse_location_grid()
+            subswath_geom = self._parse_subswath_geometry(coord_list)
+
+            df_all = gpd.GeoDataFrame([[self._target_subswath.upper()] * len(subswath_geom), subswath_geom.keys(), subswath_geom.values()], crs='EPSG:4326').T
+            df_all.columns = ['subswath', 'burst', 'geometry']
+
+        self.df = df_all
+
+    def to_json(self):
+        """
+        Returns S1-TOPS-SPLIT data in JSON format
+        :return: JSON
+        """
+        if self.df is None:
+            self._create_subswath_geometry()
+        return self.df.to_json()
+
+    def to_shapefile(self, output_file):
+        """
+        Write shapefile of S1-TOPS-SPLIT data
+        :param output_file: Path of output shapefile
+        """
+        if self.df is None:
+            self._create_subswath_geometry()
+        self.df.to_file(filename=output_file)
+
+    def to_csv(self, output_file):
+        """
+        Write CSV of S1-TOPS-SPLIT data
+        :param output_file: Path of output CSV file
+        """
+        if self.df is None:
+            self._create_subswath_geometry()
+        self.df.to_csv(output_file, index=False)
+
+    def visualize_webmap(self, polygon=None):
+        """
+        Visualize S1-TOPS data on a Folium webmap. Intended for Jupyter Notebook environments
+        :param polygon: Path of additional shapefile to visualize
+        :return: Folium webamp
+        """
+
+        if self.df is None:
+            self._create_subswath_geometry()
+
+        location_x = self.df.centroid.map(lambda p: p.x).iloc[0]
+        location_y = self.df.centroid.map(lambda p: p.y).iloc[0]
+        m = folium.Map(location=[location_y, location_x], zoom_start=8)
+
+        boundary = folium.FeatureGroup(name='Sentinel-1 Data').add_to(m)
+
+        for item in self.df.iterrows():
+            subswath = item[1]['subswath']
+            burst = item[1]['burst']
+            geom = item[1]['geometry']
+            info = f'SUBSWATH: {subswath}<br>Burst: {burst}'
+            # folium.GeoJson(data=geom, tooltip=info).add_to(m)
+            feature = folium.GeoJson(data=geom, tooltip=info).add_to(boundary)
+            # feature.add_child()
+            boundary.add_child(feature)
+
+        if polygon:
+            # Visualize additional polygon in red color
+            style = {'fillColor': '#cc0000', 'color': '#cc0000'}
+            df_mask = gpd.read_file(polygon)
+            folium.GeoJson(data=df_mask, tooltip='User loaded shapefile', style_function=lambda x: style, name='Additional Polygon').add_to(m)
+            folium.LayerControl().add_to(m)
+        return m
+
+    def close(self):
+        """
+        Close connection to ZIP file
+        """
+        self.archive.close()
