f0f19e66c6de938a23025d615bb7a910916bc1d5
pbrotoisworo <panji.p.broto@gmail.com>
2021-03-15 12:16:04 +0800

    Fixed setup py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     author='Panji P. Brotoisworo',
     url='https://github.com/pbrotoisworo/s1-tops-split-analyzer',
     version=0.1,
-    description='Interface to extract subswath data from Sentinel-1 SAR metadata'
+    description='Interface to extract subswath data from Sentinel-1 SAR metadata',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
