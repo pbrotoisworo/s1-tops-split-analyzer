2e72ea0ccffce2e23710112091dc254590df8157
pbrotoisworo <panji.p.broto@gmail.com>
2021-09-09 22:37:22 +0800

    Update documentation and installation process

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='stsa',
     author='Panji P. Brotoisworo',
     url='https://github.com/pbrotoisworo/s1-tops-split-analyzer',
-    version=0.1,
+    version=0.2,
     description='Interface to extract subswath data from Sentinel-1 SAR metadata',
     packages=find_packages(),
     include_package_data=True,
@@ -12,6 +12,20 @@ setup(
         'descartes',
         'fiona',
         'geopandas',
-        'folium'
-    ]
+        'folium',
+        'xmltodict',
+        'pytest'
+    ],
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords="SAR, Sentinel-1, Remote Sensing",
 )
