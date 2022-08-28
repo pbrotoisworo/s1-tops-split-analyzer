2eb53f4fc790e0b72df1fca4b522e0c1af948849
Panji Brotoisworo <panji.p.broto@gmail.com>
2022-08-28 19:39:57 +0800

    webapp accepts file uploads for AOI geometry

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import geopandas as gpd
-from shapely.errors import WKTReadingError
 import streamlit as st
 import streamlit_folium
 
 from stsa.stsa import TopsSplitAnalyzer
-from stsa.utils import gdf_from_wkt
+import geopandas as gpd
 
 st.set_page_config(layout="wide")
 
@@ -19,43 +18,50 @@ This web app extracts burst and subswath data from Sentinel-1 SLC data and visua
 be downloaded as GeoJSON format for viewing in GIS software. To be able to access the API please use your Copernicus
 Scihub account. If you don't have one you can make one [here](https://scihub.copernicus.eu/).
 
-This web app allows you to add one geometry overlay. You can create or format data into WKT strings using 
-[geojson.io](https://geojson.io). Create or load your geometry then save it as WKT. Then load the WKT from the text 
-file into the text box.
+This web app allows you to add one geometry overlay. You can upload any geometry that is accepted by 
+[Geopandas](https://geopandas.org/en/stable/docs/reference/api/geopandas.read_file.html).
 """)
 
-with st.form(key='api'):
-    scene = st.text_input(
-        label='Sentinel-1 SLC scene ID'
+err_form = False
+
+scene = st.text_input(
+    label='Sentinel-1 SLC scene ID'
+)
+aoi = st.file_uploader(
+label='AOI Geometry'
+)
+if aoi:
+    try:
+        geom_overlay = gpd.read_file(aoi)
+        st.success(f"File '{aoi.name}' accepted")
+    except:
+        st.error('Error: Invalid AOI file')
+        err_form = True
+else:
+    geom_overlay = None
+
+col1, col2, col3 = st.columns(3)
+with col1:
+    username = st.text_input(
+        label='Scihub Username'
     )
-    wkt_aoi = st.text_input(
-        label='WKT of geometry overlay (optional)'
+with col2:
+    password = st.text_input(
+        label='Scihub Password',
+        type='password'
     )
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        username = st.text_input(
-            label='Scihub Username'
-        )
-    with col2:
-        password = st.text_input(
-            label='Scihub Password',
-            type='password'
-        )
-    with col3:
-        pass
-    load_button = st.form_submit_button(label='Load data')
+with col3:
+    pass
+
+load_button = st.button('Load SLC Scene')
 
 if load_button:
-    
-    # Check WKT input before connecting to API
-    if wkt_aoi:
-        try:
-            geom_overlay = gdf_from_wkt(wkt_aoi)
-        except WKTReadingError:
-            st.error('Error: Failed to parse WKT string for geometry overlay.')
-            st.stop()
-    else:
-        geom_overlay = None
+
+    if err_form:
+        st.error('Please fix errors before searching.')
+        st.stop()
+
+    st.markdown("""---""") 
 
     s1 = TopsSplitAnalyzer(streamlit_mode=True, verbose=False)
     with st.spinner('Connecting to API...'):
@@ -69,9 +75,11 @@ if load_button:
         st.error(f'Error: Scene is offline')
         st.stop()
 
-    download = st.empty()
+    st.header('Search Results')
 
-    if wkt_aoi:
+    if geom_overlay is not None:
+        st.subheader('AOI Information')
+        st.dataframe(geom_overlay['geometry'].astype(str))
         df_intersect = gpd.GeoDataFrame(
             s1.intersecting_bursts(geom_overlay), columns=['subswath', 'burst']
         )
@@ -80,8 +88,9 @@ if load_button:
             st.dataframe(df_intersect)
         else:
             st.text('No overlap found with the geometry overlay.')
-    else:
-        geom_overlay = None
+
+    st.subheader('Burst Data')
+    download = st.empty()
     streamlit_folium.folium_static(s1.visualize_webmap(geom_overlay), width=1200, height=800)
 
     filename = f'{scene}.geojson'
